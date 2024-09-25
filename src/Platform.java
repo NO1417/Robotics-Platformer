98c5484350d573d88ae7628a2a43e8f241e20639
firef1a <hayden@willowmail.com>
2024-09-24 17:30:08 -0700

    moved color

--- src/Platform.java
+++ src/Platform.java
@@ -1,7 +1,7 @@
 import java.awt.*;
 
 public class Platform extends Rect {
-    public Platform(int x, int y, int width, int height) {
-        super(x,y,width,height);
+    public Platform(int x, int y, int width, int height, Color color) {
+        super(x,y,width,height, color);
     }
 }
