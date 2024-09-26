142d13966bd59af1bb5cded54cc1bfd1e4ab5791
Chris Jeon <51980572+chriswjeon@users.noreply.github.com>
2024-09-26 15:47:17 -0700

    Create KillBlock.java

---
+++ src/KillBlock.java
@@ -0,0 +1,7 @@
+import java.awt.*;
+
+public class KillBlock extends Rect {
+    public KillBlock(int x, int y, int width, int height, Color color) {
+        super(x, y, width, height, color);
+    }
+}
